e32eddec3784f3c4c3dcd9bc320b669327ac65b2
carrie <wang.carr@northeastern.edu>
2023-02-01 10:37:48 -0500

    formatted & add comments

--- starter-files/Image.cpp
+++ starter-files/Image.cpp
@@ -1,75 +1,71 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
+#include "Image.hpp"
 
-#include "Image.hpp" 
+#include <fstream>
+#include <iostream>
+#include <sstream>
 
 using namespace std;
 
-Image::Image(): m_width(0), m_height(0) {}
+Image::Image() : m_width(0), m_height(0) {}
 
-
-Image::Image(int width, int height): m_width(width), m_height(height) {
-    m_red_channel = Matrix(width, height, 0);
-    m_green_channel = Matrix(width, height, 0);
-    m_blue_channel = Matrix(width, height, 0);
+Image::Image(int width, int height) : m_width(width), m_height(height) {
+  m_red_channel = Matrix(width, height, 0);
+  m_green_channel = Matrix(width, height, 0);
+  m_blue_channel = Matrix(width, height, 0);
 }
 
-Image::Image(int width, int height, const Pixel& fill): m_width(width), m_height(height) {
-    m_red_channel = Matrix(width, height, fill.r);
-    m_green_channel = Matrix(width, height, fill.g);
-    m_blue_channel = Matrix(width, height, fill.b);
+Image::Image(int width, int height, const Pixel& fill) : m_width(width), m_height(height) {
+  m_red_channel = Matrix(width, height, fill.r);
+  m_green_channel = Matrix(width, height, fill.g);
+  m_blue_channel = Matrix(width, height, fill.b);
 }
 
 Image Image::read_ppm(std::istream& is) {
-    string magic_number; 
-    int width, height, max_color;
-    is >> magic_number >> width >> height >> max_color;
+  string magic_number;
+  int width, height, max_color;
+  is >> magic_number >> width >> height >> max_color;
 
-    Image img = Image(width, height);
+  Image img = Image(width, height);
 
-    for(int row=0; row<img.get_height(); row++) {
-        for(int col=0; col<img.get_width(); col++) {
-            int red, green, blue;
-            is >> red >> green >> blue;
-            Pixel p = Pixel{red, green, blue}; 
-            img.set_pixel(row, col, p);
-
-        }    
+  for (int row = 0; row < img.get_height(); row++) {
+    for (int col = 0; col < img.get_width(); col++) {
+      int red, green, blue;
+      is >> red >> green >> blue;
+      Pixel p = Pixel{red, green, blue};
+      img.set_pixel(row, col, p);
     }
-    return img; 
-} 
+  }
+  return img;
+}
 
 void Image::print(std::ostream& os) const {
-    os << "P3" << endl;
-    os << m_width << " " << m_height << endl;
-    os << c_max_intensity << endl;
-    // print pixel
-    for(int row=0;row<m_height; row++) {
-        for(int col=0; col<m_width; col++) {
-            os << m_red_channel.at(row, col) << " ";
-            os << m_green_channel.at(row, col) << " ";
-            os << m_blue_channel.at(row, col) << " ";
-        }
-        os << endl;
+  os << "P3" << endl;
+  os << m_width << " " << m_height << endl;
+  os << c_max_intensity << endl;
+  // print pixel
+  for (int row = 0; row < m_height; row++) {
+    for (int col = 0; col < m_width; col++) {
+      os << m_red_channel.at(row, col) << " ";
+      os << m_green_channel.at(row, col) << " ";
+      os << m_blue_channel.at(row, col) << " ";
     }
-} 
+    os << endl;
+  }
+}
 
 int Image::get_width() const { return m_width; }
 
 int Image::get_height() const { return m_height; }
 
 Pixel Image::get_pixel(int row, int column) const {
-    int r = m_red_channel.at(row, column);
-    int g = m_green_channel.at(row, column);
-    int b = m_blue_channel.at(row, column);
-    return Pixel{r, g, b};
+  int r = m_red_channel.at(row, column);
+  int g = m_green_channel.at(row, column);
+  int b = m_blue_channel.at(row, column);
+  return Pixel{r, g, b};
 }
 
 void Image::set_pixel(int row, int column, const Pixel& color) {
-    // modify the color matrix to contain the new color
-    m_red_channel.at(row, column) = color.r;
-    m_green_channel.at(row, column) = color.g;
-    m_blue_channel.at(row, column) = color.b;
+  m_red_channel.at(row, column) = color.r;
+  m_green_channel.at(row, column) = color.g;
+  m_blue_channel.at(row, column) = color.b;
 }
-
--- starter-files/Matrix.cpp
+++ starter-files/Matrix.cpp
@@ -1,82 +1,82 @@
-#include <iostream>
-#include <cassert>
-
 #include "Matrix.hpp"
+
+#include <cassert>
+#include <iostream>
 
 using namespace std;
 
 // Initializes an empty Matrix
-Matrix::Matrix(): m_width(0), m_height(0) {}
+Matrix::Matrix() : m_width(0), m_height(0) {}
 
 // Initializes a Matrix with the given dimensions and
 // with each cell initialized to zero.
-Matrix::Matrix(int width, int height): Matrix{width, height, 0} {}
+Matrix::Matrix(int width, int height) : Matrix{width, height, 0} {}
 
 // Initializes a Matrix with the given dimensions and
 // with each cell initialized to the value specified by fill_value.
-Matrix::Matrix(int width, int height, int fill_value): m_width(width), m_height(height) {
-    assert (fill_value <= 255 && fill_value >= 0);
-    for(int i = 0; i < height * width; ++i){
-        m_data.push_back(fill_value);
-    }
+Matrix::Matrix(int width, int height, int fill_value) : m_width(width), m_height(height) {
+  assert(fill_value <= 255 && fill_value >= 0);
+  for (int i = 0; i < height * width; ++i) {
+    m_data.push_back(fill_value);
+  }
 }
 
 int Matrix::get_width() const {
-    return m_width;
+  return m_width;
 }
 
 int Matrix::get_height() const {
-    return m_height;
+  return m_height;
 }
 
 int& Matrix::at(int row, int column) {
-    assert (row >= 0);
-    assert (column >= 0);
-    assert (row < m_height);
-    assert (column < m_width);
+  assert(row >= 0);
+  assert(column >= 0);
+  assert(row < m_height);
+  assert(column < m_width);
 
-    return m_data.at(get_width()*row+column);
+  return m_data.at(get_width() * row + column);
 }
 
 const int& Matrix::at(int row, int column) const {
-    assert(row>=0);
-    assert(column>=0);
-    assert(row<m_height);
-    assert(column<m_width);
-    return m_data.at(get_width()*row+column);
+  assert(row >= 0);
+  assert(column >= 0);
+  assert(row < m_height);
+  assert(column < m_width);
+
+  return m_data.at(get_width() * row + column);
 }
 
 Matrix::Slice Matrix::get_row_slice(int row, int col_start, int col_end) const {
-    assert(col_start<col_end);
-    assert(row>=0);
-    assert(row<m_height);
+  assert(col_start < col_end);
+  assert(row >= 0);
+  assert(row < m_height);
 
-    vector<int> data;
-    Slice s = Slice{data, row, col_start, col_end};
+  vector<int> data;
+  Slice s = Slice{data, row, col_start, col_end};
 
-    if(col_start < 0) { // ret slice, first element is 0
-        col_start = 0;
-    }
-    if(col_end > m_width) { // ret slice is last element
-        col_end = m_width;
-    }
+  if (col_start < 0) {  // ret slice, first element is 0
+    col_start = 0;
+  }
+  if (col_end > m_width) {  // ret slice is last element
+    col_end = m_width;
+  }
 
-    // row is height, col start/end is width, so get a slice @ a row
-    for(int i = col_start; i<=col_end; i++) {
-        s.data.push_back(at(row, i));
-    }
-    return s;
+  // row is height, col start/end is width, so get a slice @ a row
+  for (int i = col_start; i <= col_end; i++) {
+    s.data.push_back(at(row, i));
+  }
+  return s;
 }
 
 void Matrix::print(std::ostream& os) const {
-    os << m_width << " " << m_height << endl;
-    int index = 0;
-    for(int row = 0; row<m_height; row++) {
-        for(int col = 0; col<m_width; col++) {
-            // better to use .at(index) bc it'll give an error when go oob for [index]
-            os << m_data.at(index) << " "; 
-            index++;
-        }
-        os << endl;
+  os << m_width << " " << m_height << endl;
+  int index = 0;
+  for (int row = 0; row < m_height; row++) {
+    for (int col = 0; col < m_width; col++) {
+      os << m_data.at(index) << " ";
+      index++;
     }
+    os << endl;
+  }
 }
