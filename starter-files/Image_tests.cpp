c57f047cb299e46c66953a83df3000c2e48b22e8
carrie <wang.carr@northeastern.edu>
2023-01-31 23:54:44 -0500

    remove duplication from image test

--- starter-files/Image_tests.cpp
+++ starter-files/Image_tests.cpp
@@ -9,36 +9,6 @@
 
 using namespace std;
 
-// Here's a free test for you! Model yours after this one.
-// Add a comment like the one here to say what it is testing.
-// -----
-// Sets various pixels in a 2x2 Image and checks
-// that Image::print produces the correct output.
-TEST(test_print_basic)
-{
-  const Pixel red = {255, 0, 0};
-  const Pixel green = {0, 255, 0};
-  const Pixel blue = {0, 0, 255};
-  const Pixel white = {255, 255, 255};
-
-  Image img(2, 2);
-  img.set_pixel(0, 0, red);
-  img.set_pixel(0, 1, green);
-  img.set_pixel(1, 0, blue);
-  img.set_pixel(1, 1, white);
-
-  // Capture our output
-  ostringstream actual;
-  img.print(actual);
-
-  // Correct output
-  ostringstream correct;
-  correct << "P3\n2 2\n255\n";
-  correct << "255 0 0 0 255 0 \n";
-  correct << "0 0 255 255 255 255 \n";
-  ASSERT_EQUAL(correct.str(), actual.str());
-}
-
 // ADD YOUR TESTS HERE
 // You are encouraged to use any functions from Image_test_helpers.h as needed.
 TEST(test_default_constructor)
